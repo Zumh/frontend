56bc013299b47a4ae2ac721d4fe8e95d2804f452
Zumh <17211423+Zumh@users.noreply.github.com>
2023-08-06 21:13:13 -0400

    update fronts end

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,15 +1,31 @@
 import axios from 'axios'
 //const baseUrl = 'http://localhost:3001/notes'
 const baseUrl = '/api/notes';
+
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
+const create = async newObject => {
+  // const request = axios.post(baseUrl, newObject)
+  // return request.then(response => response.data)
+
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
 }
+
+
 
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
@@ -19,5 +35,6 @@ const update = (id, newObject) => {
 export default { 
   getAll, 
   create, 
-  update 
+  update,
+  setToken 
 }
